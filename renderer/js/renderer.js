57ca7d71a8efa6d5e4bf26ae0ed07d5d1d284065
Reynard <reyntors2@gmail.com>
2023-05-13 13:16:20 +0800

    Set Html Structure

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,2 +1,14 @@
-const information = document.getElementById("info");
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+const generateButton = document.getElementById("generate");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementsByClassName("close")[0];
+const textarea = document.getElementById("note-textarea");
+
+generateButton.addEventListener("click", () => {
+  const text = textarea.value;
+  // perform keyword extraction and display results in modal
+  // ...
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
